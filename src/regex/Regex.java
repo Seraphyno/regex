6e54a5cb70959698deffd070a7d09b6286bdcba7
Serafino Tudorovici <serafino.radulescu@gmail.com>
2019-05-10 23:08:37 +0300

    first commit

---
+++ src/regex/Regex.java
@@ -0,0 +1,74 @@
+package regex;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/*
+ * https://www.javatpoint.com/java-regex
+ *
+ * Capturing groups: https://www.logicbig.com/tutorials/core-java-tutorial/java-regular-expressions/named-captruing-groups.html
+ */
+public class Regex {
+    
+    public static void main(String[] args) {
+        System.out.println(Pattern.matches(".s", "as"));//true (2nd char is s)
+        System.out.println(Pattern.matches(".s", "mk"));//false (2nd char is not s)
+        System.out.println(Pattern.matches(".s", "mst"));//false (has more than 2 char)
+        System.out.println(Pattern.matches(".s", "amms"));//false (has more than 2 char)
+        System.out.println(Pattern.matches("..s", "mas"));//true (3rd char is s)
+        
+        System.out.println(Pattern.matches("[amn]", "abcd"));//false (not a or m or n)
+        System.out.println(Pattern.matches("[amn]", "a"));//true (among a or m or n)
+        System.out.println(Pattern.matches("[amn]", "ammmna"));//false (m and a comes more than once)
+        
+        System.out.println("? quantifier ....");
+        System.out.println(Pattern.matches("[amn]?", "a"));//true (a or m or n comes one time)
+        System.out.println(Pattern.matches("[amn]?", "aaa"));//false (a comes more than one time)
+        System.out.println(Pattern.matches("[amn]?", "aammmnn"));//false (a m and n comes more than one time)
+        System.out.println(Pattern.matches("[amn]?", "aazzta"));//false (a comes more than one time)
+        System.out.println(Pattern.matches("[amn]?", "am"));//false (a or m or n must come one time)
+        
+        System.out.println("+ quantifier ....");
+        System.out.println(Pattern.matches("[amn]+", "a"));//true (a or m or n once or more times)
+        System.out.println(Pattern.matches("[amn]+", "aaa"));//true (a comes more than one time)
+        System.out.println(Pattern.matches("[amn]+", "aammmnn"));//true (a or m or n comes more than once)
+        System.out.println(Pattern.matches("[amn]+", "aazzta"));//false (z and t are not matching pattern)
+        
+        System.out.println("* quantifier ....");
+        System.out.println(Pattern.matches("[amn]*", "ammmna"));//true (a or m or n may come zero or more times)
+        
+        System.out.println("metacharacters d....");//d means digit
+        
+        System.out.println(Pattern.matches("\\d", "abc"));//false (non-digit)
+        System.out.println(Pattern.matches("\\d", "1"));//true (digit and comes once)
+        System.out.println(Pattern.matches("\\d", "4443"));//false (digit but comes more than once)
+        System.out.println(Pattern.matches("\\d", "323abc"));//false (digit and char)
+        
+        System.out.println("metacharacters D....");//D means non-digit
+        
+        System.out.println(Pattern.matches("\\D", "abc"));//false (non-digit but comes more than once)
+        System.out.println(Pattern.matches("\\D", "1"));//false (digit)
+        System.out.println(Pattern.matches("\\D", "4443"));//false (digit)
+        System.out.println(Pattern.matches("\\D", "323abc"));//false (digit and char)
+        System.out.println(Pattern.matches("\\D", "m"));//true (non-digit and comes once)
+        
+        System.out.println("metacharacters D with quantifier....");
+        System.out.println(Pattern.matches("\\D*", "mak"));//true (non-digit and may come 0 or more times)
+        
+        String text =
+                    "This is the text which is to be searched " +
+                                "for occurrences of the word 'is'.";
+        
+        String regex = "is";
+        
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(text);
+        
+        int count = 0;
+        while (matcher.find()) {
+            count++;
+            System.out.println("found: " + count + " : "
+                        + matcher.start() + " - " + matcher.end());
+        }
+    }
+}
